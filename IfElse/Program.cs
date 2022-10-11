62a7d7ba941d9c16ab2d3ffc6a88bb71c9290254
IrinaTsagaeva <irina-hadaeva@yandex.ru>
2022-10-11 22:14:45 +0300

    add condition If Else

---
+++ IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Please, enter your name");
+string username = Console.ReadLine();
+if (username.ToLower()=="vit")
+{
+    Console.WriteLine ("I love you "+ username);
+}
+else
+{
+    Console.WriteLine("Hello, "+ username);
+}
+
